dc91ddc23708a2bc34d92771882e3caf4ef6f5cf
tanbensong <49031955+tanbensong@users.noreply.github.com>
2021-12-29 14:32:23 +0800

    Update models.py

--- restore/models.py
+++ restore/models.py
@@ -2,17 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class serverDatabase(models.Model):
-    name = models.CharField(max_length=40, unique=True, verbose_name="数据库名称")
+class ServerDatabase(models.Model):
+    dataName = (
+        (0, "MySQL"),
+        (1, "SQL Server"),
+        (2, "MongoDB"),
+        (3, "PostgreSQL"),
+    )
+
+    name = models.SmallIntegerField(max_length=40, choices=dataName, verbose_name="数据库")
     post = models.IntegerField(validators=[], verbose_name="数据库端口")
     user = models.CharField(max_length=40, verbose_name="数据库用户")
     passwd = models.CharField(max_length=40, verbose_name="数据库密码")
-    database = models.CharField(max_length=40, verbose_name="数据库名称")
+    database = models.CharField(max_length=40, unique=True, verbose_name="数据库名称")
+    remarks = models.CharField(max_length=20, null=True, blank=True, verbose_name="备注")
 
     def __str__(self):
         return str(self.name)
 
     class Meta:
         db_table = "tb_server_databases"
-        verbose_name = "服务器程序"
+        verbose_name = "数据库信息"
         verbose_name_plural = verbose_name
